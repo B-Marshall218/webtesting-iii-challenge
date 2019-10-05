8cf7a36434b2a989d15dd7c9c95f1c7dd8c76961
Brian <lawmen218@yahoo.com>
2019-10-05 15:11:31 -0400

    mvp

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -19,18 +19,19 @@ test("Buttons text changes to reflect the state the door will be in if clicked",
     const { getByText } = render(<Controls
         locked={true}
         closed={true}
+
     />)
 
 
-    const toggleLocked = getByText(/'unlock gate'/i);
-    const toggleClosed = getByText(/"open gate"/i);
+    const toggleLocked = getByText(/unlock gate/i);
+    const toggleClosed = getByText(/open gate/i);
 
     expect(toggleLocked).toBeTruthy()
     expect(toggleClosed).toBeTruthy()
 
 })
 
-test("Button disabled if gate is locked", () => {
+test("Closed Button disabled if gate is locked", () => {
     const mockToggleClosed = jest.fn()
 
     const { queryByText } = render(<Controls
@@ -41,7 +42,28 @@ test("Button disabled if gate is locked", () => {
     );
 
 
-    const lockedBtn = queryByText(/open gate/i);
+
+
+    const closedBtn = queryByText(/open gate/i);
+    expect(closedBtn).toBeTruthy()
+
+    fireEvent.click(closedBtn);
+})
+
+test("Locked Button disabled if gate is open", () => {
+    const mockToggleLocked = jest.fn()
+
+    const { getByText } = render(<Controls
+        unlocked={true}
+        open={true}
+        toggleLocked={mockToggleLocked}
+    />
+    );
+
+
+
+
+    const lockedBtn = getByText(/lock gate/i);
     expect(lockedBtn).toBeTruthy()
 
     fireEvent.click(lockedBtn);
